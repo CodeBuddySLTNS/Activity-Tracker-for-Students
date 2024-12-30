115dd8c357c5366bd7dce93974c1c2dab00aa68e
CodeBuddySLTNS <renz2arjhay@gmail.com>
2024-12-30 13:23:14 +0800

    update script.js

--- script.js
+++ script.js
@@ -22,70 +22,71 @@ activities = activities ? JSON.parse(activities) : [];
 // display the activities
 displayActivities();
 
+// toggle the activity form if no activities
+if (activities.length < 1) formToggler()
+
 // add a callback function to handle form submit
 newForm.onsubmit = addActivity;
 
 function displayActivities() {
-  if (activities.length > 0) {
-    document.querySelector("#cards").innerHTML = "";
-    document.querySelector("#all").innerHTML = activities.length;
-    document.querySelector("#incomplete").innerHTML = activities.filter(
-      a => !a.status
-    ).length;
-    document.querySelector("#complete").innerHTML = activities.filter(
-      a => a.status
-    ).length;
+  document.querySelector("#cards").innerHTML = "";
+  document.querySelector("#all").innerHTML = activities.length;
+  document.querySelector("#incomplete").innerHTML = activities.filter(
+    a => !a.status
+  ).length;
+  document.querySelector("#complete").innerHTML = activities.filter(
+    a => a.status
+  ).length;
 
-    activities.forEach(activity => {
-      document.querySelector("#cards").innerHTML += `
-        <div class="card">
-          <h4 class="card-label-w-icon">
-            ${activity.activity}
-            <i class="fa-solid fa-trash text-red-600" onclick="deleteActivity('${
-              activity.activity
-            }')"></i>
-          </h4>
-          <pre class="card-description">${
-            activity.description ? activity.description : "No description."
-          }</pre
-          >
-          <div class="my-1 flex justify-between items-center gap-1">
-            <p>Due: ${activity.due}</p>
-            <div class="${
-              (activity.status || dueDateChecker(activity.due) == "future") &&
-              "hidden"
-            } flex items-center gap-1 mr-0.5 text-sm text-red-500">
-              ${
-                dueDateChecker(activity.due) == "past"
-                  ? "<span>overdue</span>"
-                  : "<span>due today</span>"
-              }
-              <i class="fa-solid fa-triangle-exclamation"></i>
-            </div>
-          </div>
-          <div class="mt-1 grid grid-cols-2 gap-2">
-            <button class="button bg-blue-200 hover:bg-gray-100 hover:text-black" onclick="editActivity('${
-              activity.activity
-            }')">
-              <i class="fa-solid fa-pen-to-square"></i>
-              Edit
-            </button>
+  activities.forEach(activity => {
+    document.querySelector("#cards").innerHTML += `
+      <div class="card">
+        <h4 class="card-label-w-icon">
+          ${activity.activity}
+          <i class="fa-solid fa-trash text-red-600" onclick="deleteActivity('${
+            activity.activity
+          }')"></i>
+        </h4>
+        <pre class="card-description">${
+          activity.description ? activity.description : "No description."
+        }</pre
+        >
+        <div class="my-1 flex justify-between items-center gap-1">
+          <p>Due: ${activity.due}</p>
+          <div class="${
+            (activity.status || dueDateChecker(activity.due) == "future") &&
+            "hidden"
+          } flex items-center gap-1 mr-0.5 text-sm text-red-500">
             ${
-              activity.status
-                ? `<button class="button bg-green-100 text-green-700 hover:bg-gray-100 hover:text-black" onclick="changeStatus('${activity.activity}')">
-              <i class="fa-solid fa-check"></i>
-              Completed
-            </button>`
-                : `<button class="button bg-red-100 flex justify-center gap-1.5 items-center hover:bg-gray-100 hover:text-black" onclick="changeStatus('${activity.activity}')">
-              <i class="fa-solid fa-xmark"></i>
-              Incomplete
-            </button>`
+              dueDateChecker(activity.due) == "past"
+                ? "<span>overdue</span>"
+                : "<span>due today</span>"
             }
+            <i class="fa-solid fa-triangle-exclamation"></i>
           </div>
         </div>
-      `;
-    });
-  }
+        <div class="mt-1 grid grid-cols-2 gap-2">
+          <button class="button bg-blue-200 hover:bg-gray-100 hover:text-black" onclick="editActivity('${
+            activity.activity
+          }')">
+            <i class="fa-solid fa-pen-to-square"></i>
+            Edit
+          </button>
+          ${
+            activity.status
+              ? `<button class="button bg-green-100 text-green-700 hover:bg-gray-100 hover:text-black" onclick="changeStatus('${activity.activity}')">
+            <i class="fa-solid fa-check"></i>
+            Completed
+          </button>`
+              : `<button class="button bg-red-100 flex justify-center gap-1.5 items-center hover:bg-gray-100 hover:text-black" onclick="changeStatus('${activity.activity}')">
+            <i class="fa-solid fa-xmark"></i>
+            Incomplete
+          </button>`
+          }
+        </div>
+      </div>
+    `;
+  });
 }
 
 function addActivity(e) {
